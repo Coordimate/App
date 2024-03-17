78fc6cd90792fffa2dcea7b3158c1fd7016e5337
VladaPri <35739172+Lamparu@users.noreply.github.com>
2024-03-17 15:37:58 +0100

    changed main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
+import 'package:coordimate/pages/start_page.dart';
 import 'package:flutter/material.dart';
-// import 'pages/login_page.dart';
-// import 'pages/start_page.dart';
-import 'pages/register_page.dart';
-import 'pages/groups_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,26 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(fontFamily: 'OpenSans'),
-        home: GroupsPage());
-  }
-}
-*/
-import 'package:flutter/material.dart';
-import 'pages/groups_page.dart'; // Import only the groups page
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'OpenSans'),
-      home: GroupsPage(), // Set the groups page as the home page
+        home: StartPage()
     );
   }
 }
