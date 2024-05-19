dca10bd363221dc36db1bbaa46f2f848e3513953
AFKabl3 <thisismyworkhotmail@gmail.com>
2024-05-19 22:03:25 +0300

    Co-authored-by: Valentin Vikhorev <vvihorev@users.noreply.github.com>

--- lib/pages/groups_page.dart
+++ lib/pages/groups_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:coordimate/components/colors.dart';
+import 'package:coordimate/components/appbar.dart';
+import 'package:coordimate/models/groups.dart';
+import 'package:coordimate/keys.dart';
+
+import 'dart:convert';
+import 'package:coordimate/api_client.dart';
 
 class GroupsPage extends StatefulWidget {
   const GroupsPage({super.key});
@@ -10,14 +16,48 @@ class GroupsPage extends StatefulWidget {
 }
 
 class _GroupsPageState extends State<GroupsPage> {
-  final List<String> _notes = [];
+  Future<List<Group>>? _groupsFuture;
+  List<Group> _groups = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _groupsFuture = _getGroups();
+  }
+
+  Future<List<Group>> _getGroups() async {
+    final response = await client.get(Uri.parse("$apiUrl/groups/"));
+    final List body = json.decode(response.body)["groups"];
+    setState(() {
+      _groups = body.map((e) => Group.fromJson(e)).toList();
+    });
+    return _groups;
+  }
+
+  Future<void> _createGroup(String name, String description) async {
+    final response = await client.post(
+      Uri.parse("$apiUrl/groups/"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'name': name,
+        'description': description,
+      }),
+    );
+    if (response.statusCode == 201) {
+      _getGroups();
+    } else {
+      throw Exception('Failed to create meeting');
+    }
+  }
 
   _showAlertDialog() {
     showDialog(
       context: context,
       builder: (context) {
-        String newNoteTitle = ''; // Variable to store the entered title
-        String newNoteDescription =
+        String groupName = ''; // Variable to store the entered title
+        String groupDescription =
             ''; // Variable to store the entered description
 
         return AlertDialog(
@@ -32,7 +72,7 @@ class _GroupsPageState extends State<GroupsPage> {
               children: [
                 TextFormField(
                   onChanged: (val) {
-                    newNoteTitle =
+                    groupName =
                         val; // Update the newNoteTitle variable with the entered title
                   },
                   decoration: InputDecoration(
@@ -51,7 +91,7 @@ class _GroupsPageState extends State<GroupsPage> {
                     labelStyle: const TextStyle(
                         color: Colors.white), // Change label color to white
                     counterText:
-                        '${newNoteTitle.length}/20', // Add character counter
+                        '${groupName.length}/20', // Add character counter
                     counterStyle: const TextStyle(
                         color:
                             Colors.white), // Change counter text color to white
@@ -65,7 +105,7 @@ class _GroupsPageState extends State<GroupsPage> {
                         10), // Add some space between title and description fields
                 TextFormField(
                   onChanged: (val) {
-                    newNoteDescription =
+                    groupDescription =
                         val; // Update the newNoteDescription variable with the entered description
                   },
                   maxLines:
@@ -88,7 +128,7 @@ class _GroupsPageState extends State<GroupsPage> {
                     labelStyle: const TextStyle(
                         color: Colors.white), // Change label color to white
                     counterText:
-                        '${newNoteDescription.length}/300', // Add character counter
+                        '${groupDescription.length}/300', // Add character counter
                     counterStyle: const TextStyle(
                         color:
                             Colors.white), // Change counter text color to white
@@ -108,14 +148,10 @@ class _GroupsPageState extends State<GroupsPage> {
                 mainAxisAlignment: MainAxisAlignment.end, // Align to the right
                 children: [
                   OutlinedButton(
-                    onPressed: () {
-                      if (newNoteTitle.isNotEmpty &&
-                          newNoteTitle.length <= 20) {
-                        setState(() {
-                          _notes.add(
-                              '$newNoteTitle\n$newNoteDescription'); // Add the new note to the list with title and description
-                        });
-                        Navigator.pop(context);
+                    onPressed: () async {
+                      if (groupName.isNotEmpty && groupName.length <= 20) {
+                        await _createGroup(groupName, groupDescription);
+                        Navigator.of(context).pop();
                       }
                     },
                     child: const Text(
@@ -136,132 +172,83 @@ class _GroupsPageState extends State<GroupsPage> {
 
   _resetNotes() {
     setState(() {
-      _notes.clear(); // Clear the list to reset notes
+      _groups.clear(); // Clear the list to reset notes
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize:
-            const Size.fromHeight(kToolbarHeight + 20), // Keep the same height
-        child: Container(
-          padding: const EdgeInsets.only(
-              top: 20), // Indent the app bar from the top by 20 pixels
-          child: AppBar(
-            backgroundColor: Colors.white, // Set background color to white
-            title: Stack(
-              children: [
-                const Center(
-                  child: Text(
-                    'GROUPS',
-                    style: TextStyle(
-                      color: darkBlue, // Hex color value 293241
-                      fontWeight: FontWeight.w900,
-                      fontSize: 24, // Adjust font size as needed
+    return FutureBuilder(
+        future: _groupsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Scaffold(
+              appBar: CustomAppBar(
+                  title: "Groups",
+                  needButton: true,
+                  onPressed: _showAlertDialog),
+              body: ListView.builder(
+                itemCount: _groups.length,
+                itemBuilder: (context, index) {
+                  // Splitting the note into title and description
+                  return Container(
+                    decoration: BoxDecoration(
+                      color:
+                          darkBlue, // Set background color of the card to grey
+                      borderRadius: BorderRadius.circular(
+                          10), // Add border radius to make it look like a card
                     ),
-                  ),
-                ),
-                Positioned(
-                  right: 0,
-                  bottom: 0,
-                  top: 0,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      FloatingActionButton(
-                        onPressed: _showAlertDialog,
-                        tooltip: 'Add Group',
-                        backgroundColor:
-                            darkBlue, // Set background color to hex value 3D5A80
-                        elevation: 0, // Remove shadow
-                        child: Image.asset(
-                          'lib/images/create.png', // Path to custom icon
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 70, // Set the width of the circle
+                          height: 70, // Set the height of the circle
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle, // Make it a circle
+                            color: Colors.white, // Set color to white
+                          ),
                         ),
-                      ),
-                      const SizedBox(width: 8),
-                      FloatingActionButton(
-                        onPressed: _resetNotes,
-                        tooltip: 'Reset Groups',
-                        backgroundColor:
-                            darkBlue, // Set background color to hex value 3D5A80
-                        elevation: 0, // Remove shadow
-                        child: const Icon(
-                          Icons.replay,
-                          color: Colors.white, // Set icon color to white
+                        const SizedBox(
+                            width:
+                                10), // Add some space between the circle and text
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                _groups[index].name,
+                                style: const TextStyle(
+                                  fontSize: 30, // Font size for title
+                                  color: Colors.white,
+                                  fontWeight:
+                                      FontWeight.bold, // Make the title bold
+                                ),
+                              ),
+                              const SizedBox(
+                                  height:
+                                      5), // Add some vertical space between title and description
+                              Text(
+                                _groups[index].description,
+                                style: const TextStyle(
+                                  fontSize: 16, // Font size for description
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            centerTitle: true, // Centers the title within the app bar
-          ),
-        ),
-      ),
-      body: ListView.builder(
-        itemCount: _notes.length,
-        itemBuilder: (context, index) {
-          // Splitting the note into title and description
-          List<String> noteParts = _notes[index].split('\n');
-          String title = noteParts[0];
-          String description = noteParts.length > 1
-              ? noteParts[1]
-              : ''; // Ensure description exists, handle if not
-
-          return Container(
-            decoration: BoxDecoration(
-              color: darkBlue, // Set background color of the card to grey
-              borderRadius: BorderRadius.circular(
-                  10), // Add border radius to make it look like a card
-            ),
-            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Container(
-                  width: 70, // Set the width of the circle
-                  height: 70, // Set the height of the circle
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle, // Make it a circle
-                    color: Colors.white, // Set color to white
-                  ),
-                ),
-                const SizedBox(
-                    width: 10), // Add some space between the circle and text
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        style: const TextStyle(
-                          fontSize: 30, // Font size for title
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold, // Make the title bold
-                        ),
-                      ),
-                      const SizedBox(
-                          height:
-                              5), // Add some vertical space between title and description
-                      Text(
-                        description,
-                        style: const TextStyle(
-                          fontSize: 16, // Font size for description
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
+          } else {
+            return Text("Oh Shit!");
+          }
+        });
   }
 }
