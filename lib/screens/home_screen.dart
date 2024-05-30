f0268239eead1e723df54115642144fa3a70c687
vvihorev <vvihorevv@gmail.com>
2024-05-30 23:06:38 +0200

    Use deeplinks to invite users to groups!

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,9 +6,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:uni_links/uni_links.dart';
 import 'package:coordimate/components/colors.dart';
+import 'package:coordimate/components/join_group_dialog.dart';
 import 'package:coordimate/pages/personal_schedule_page.dart';
 import 'package:coordimate/pages/meetings_page.dart';
 import 'package:coordimate/pages/groups_page.dart';
+import 'package:coordimate/models/groups.dart';
 import 'package:coordimate/api_client.dart';
 import 'package:coordimate/keys.dart';
 
@@ -41,11 +43,11 @@ class HomeScreenState extends State<HomeScreen> {
     try {
       final initialUri = await getInitialUri();
       if (initialUri != null) {
-        _handleDeepLink(initialUri);
+        await _handleDeepLink(initialUri);
       }
-      _sub = uriLinkStream.listen((Uri? link) {
+      _sub = uriLinkStream.listen((Uri? link) async {
         if (link != null) {
-          _handleDeepLink(link);
+          await _handleDeepLink(link);
         }
       }, onError: (err) {
         print("Error parsing change in uniLink");
@@ -56,20 +58,39 @@ class HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void _handleDeepLink(Uri uri) {
+  Future<void> _handleDeepLink(Uri uri) async {
     setState(() {
       switch (uri.path) {
         case '/schedule':
           _selectedScreen = 0;
-          break;
+          return;
         case '/meetings':
           _selectedScreen = 1;
-          break;
+          return;
         case '/groups':
           _selectedScreen = 2;
-          break;
+          return;
       }
     });
+    await _tryParseGroupId(uri);
+  }
+
+  Future<void> _tryParseGroupId(Uri uri) async {
+    final regex = RegExp(r'^/groups/([0-9a-z]+)/join$');
+    final match = regex.firstMatch(uri.path);
+    if (match != null) {
+      final groupId = match.group(1)!;
+      final response = await client.get(Uri.parse("$apiUrl/groups/$groupId"));
+      if (response.statusCode == 200) {
+        final group = Group.fromJson(json.decode(response.body));
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return JoinGroupDialog(
+                  key: UniqueKey(), groupName: group.name, groupId: group.id);
+            });
+      }
+    }
   }
 
   void _onButtonPressed(int index) {
