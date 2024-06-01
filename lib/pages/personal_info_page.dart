b0a206e63468e1570e2ad6e611ef50ec5e1d7ad6
VladaPri <35739172+Lamparu@users.noreply.github.com>
2024-06-01 11:52:37 +0200

    Added editing icon and textfield for username

--- lib/pages/personal_info_page.dart
+++ lib/pages/personal_info_page.dart
@@ -1,6 +1,7 @@
 import 'package:coordimate/components/appbar.dart';
 import 'package:coordimate/components/colors.dart';
 import 'package:coordimate/components/login_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:coordimate/pages/start_page.dart';
 import 'package:coordimate/data/storage.dart';
@@ -19,11 +20,16 @@ class PersonalPage extends StatefulWidget {
 class _PersonalPageState extends State<PersonalPage> {
 
   late User user;
+  bool isEditing = false;
+  final usernameController = TextEditingController();
+
 
   @override
   void initState() {
     super.initState();
     // getInfo(); // TODO: Uncomment this line
+    // usernameController.text = user.username; // TODO: Uncomment this line
+    usernameController.text = "John Doe";
   }
 
   void logOut(BuildContext context) {
@@ -46,38 +52,91 @@ class _PersonalPageState extends State<PersonalPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        title: 'Personal Page',
-        needButton: false
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: const NetworkImage('https://www.w3schools.com/w3images/avatar2.png'),
-            ),
-            const SizedBox(height: 16),
-            const Text('John Doe', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: darkBlue)),
-            const SizedBox(height: 8),
-            const Text('string@email.com', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: darkBlue)),
-            const SizedBox(height: 16),
 
-            LoginButton(
-                text: "Logout",
-                onTap: () { logOut(context); }
-            ),
-            const SizedBox(height: 8),
-            LoginEmptyButton(
-                text: "Change Password",
-                onTap: (){}
-            ),
-          ],
-        ),
+    final textSpan = TextSpan(
+      text: usernameController.text,
+      style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: darkBlue),
+    );
+    final textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+    textPainter.layout();
+    final textWidth = textPainter.width;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: const CustomAppBar(
+          title: 'Personal Page',
+          needButton: false
       ),
-      
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 70,
+                    backgroundImage: const NetworkImage('https://www.w3schools.com/w3images/avatar2.png'),
+                  ),
+                  const SizedBox(height: 16),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 26.0),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        TextField(
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration(border: InputBorder.none,),
+                          controller: usernameController,
+                          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: darkBlue)),
+                        Positioned(
+                          right: 0,
+                          child:
+                          IconButton(
+                            icon: const Icon(Icons.edit, color: darkBlue),
+                            onPressed: () {
+                              // Implement your change username functionality here
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  const Text('string@email.com', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: darkBlue)),
+                  const SizedBox(height: 16),
+
+                  LoginEmptyButton(
+                      text: "Change Password",
+                      onTap: (){}
+                  ),
+                  const SizedBox(height: 8),
+                  LoginButton(
+                      text: "Logout",
+                      onTap: () { logOut(context); }
+                  ),
+
+
+                ],
+              ),
+            ),
+          ),
+          Padding(padding: const EdgeInsets.only(bottom: 16),
+              child: TextButton(
+                onPressed: () {
+                  // Implement your delete account functionality here
+                },
+                child: const Text(
+                  'Delete Account',
+                  style: TextStyle(color: mediumBlue, fontSize: 20, fontWeight: FontWeight.w500),
+                ),
+              )),
+        ],
+      ),
     );
   }
 }
