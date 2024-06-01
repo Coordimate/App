a72a55ea9cae7ff3cace165b535a4ddda800fa03
AFKabl3 <thisismyworkhotmail@gmail.com>
2024-06-02 01:16:00 +0300

    conncted group id to create meetings in groups detauils page

--- lib/pages/group_details_page.dart
+++ lib/pages/group_details_page.dart
@@ -6,6 +6,7 @@ import 'package:coordimate/models/meeting.dart';
 import 'package:coordimate/keys.dart';
 import 'dart:convert';
 import 'package:coordimate/api_client.dart';
+import 'package:intl/intl.dart';
 
 class GroupDetailsPage extends StatefulWidget {
   final Group group;
@@ -18,6 +19,9 @@ class GroupDetailsPage extends StatefulWidget {
 
 class _GroupDetailsPageState extends State<GroupDetailsPage> {
   List<MeetingTileModel> meetings = [];
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
 
   @override
   void initState() {
@@ -39,6 +43,159 @@ class _GroupDetailsPageState extends State<GroupDetailsPage> {
     } else {
       throw Exception('Failed to load meetings');
     }
+  }
+
+  Future<void> _selectDate() async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime(2022, 1),
+      lastDate: DateTime(2025),
+    );
+    if (!mounted) {
+      return;
+    }
+    if (pickedDate != null && pickedDate != _selectedDate) {
+      final TimeOfDay? pickedTime = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.fromDateTime(_selectedDate),
+      );
+      if (pickedTime != null) {
+        setState(() {
+          _selectedDate = DateTime(
+            pickedDate.year,
+            pickedDate.month,
+            pickedDate.day,
+            pickedTime.hour,
+            pickedTime.minute,
+          );
+        });
+      }
+    }
+  }
+
+  // Future<String?> _fetchGroupId(String groupId) async {
+  //   final response = await client.get(Uri.parse("$apiUrl/groups/$groupId"));
+  //   if (response.statusCode == 200) {
+  //     final group = json.decode(response.body);
+  //     return group['_id'];
+  //   } else {
+  //     throw Exception('Failed to load group');
+  //   }
+  // }
+
+  // Future<void> _createMeeting(String groupId) async {
+  //   String? actualGroupId = await _fetchGroupId(groupId);
+  //   if (actualGroupId != null) {
+  //     final response = await client.post(
+  //       Uri.parse("$apiUrl/meetings/"),
+  //       headers: <String, String>{
+  //         'Content-Type': 'application/json; charset=UTF-8',
+  //       },
+  //       body: jsonEncode(<String, dynamic>{
+  //         'title': _titleController.text,
+  //         'start': _selectedDate.toIso8601String(),
+  //         'description': _descriptionController.text,
+  //         'group_id': group.id,
+  //       }),
+  //     );
+  //     if (response.statusCode == 201) {
+  //       _fetchMeetings();
+  //     } else {
+  //       throw Exception('Failed to create meeting');
+  //     }
+  //   } else {
+  //     throw Exception('Failed to fetch group ID');
+  //   }
+  // }
+
+  Future<void> _createMeeting() async {
+    final response = await client.post(
+      Uri.parse("$apiUrl/meetings/"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'title': _titleController.text,
+        'start': _selectedDate.toIso8601String(),
+        'description': _descriptionController.text,
+        'group_id': widget.group.id,
+      }),
+    );
+    if (response.statusCode == 201) {
+      _fetchMeetings();
+    } else {
+      throw Exception('Failed to create meeting');
+    }
+  }
+
+  void clearControllers() {
+    _titleController.clear();
+    _descriptionController.clear();
+    _selectedDate = DateTime.now();
+  }
+
+  void _onCreateMeeting() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return AlertDialog(
+              title: const Text('Create Meeting'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Title',
+                        hintText: 'Enter the title of the meeting',
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(DateFormat('EEE, MMMM d, HH:mm')
+                          .format(_selectedDate.toLocal())),
+                      trailing: const Icon(Icons.keyboard_arrow_down),
+                      onTap: () async {
+                        await _selectDate();
+                        setState(
+                            () {}); // Rebuild the dialog to update the date
+                      },
+                    ),
+                    TextField(
+                      controller: _descriptionController,
+                      decoration: const InputDecoration(
+                        labelText: 'Description',
+                        hintText: 'Enter the description of the meeting',
+                      ),
+                      maxLines: 3,
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('Cancel'),
+                  onPressed: () {
+                    clearControllers();
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: const Text('Create'),
+                  onPressed: () {
+                    _createMeeting();
+                    clearControllers();
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
   }
 
   @override
@@ -111,7 +268,7 @@ class _GroupDetailsPageState extends State<GroupDetailsPage> {
                         .add_circle_outline_rounded), // Replace with your second button icon
                     iconSize: 43.0, // Adjust size as needed
                     onPressed: () {
-                      // Add functionality for the second button
+                      _onCreateMeeting();
                     },
                   ),
                 ],
@@ -137,7 +294,10 @@ class _GroupDetailsPageState extends State<GroupDetailsPage> {
               ),
               SizedBox(height: 16.0), // Added spacing
               if (acceptedFutureMeetings.isNotEmpty)
-                _buildMeetingList(acceptedFutureMeetings, "Upcoming Meetings"),
+                _buildMeetingList(acceptedFutureMeetings, "Upcoming Meetings")
+              else
+                _buildMeetingList(
+                    acceptedFutureMeetings, "No Upcoming Meetings"),
               SizedBox(height: 16.0), // Added spacing
               Container(
                 width: double.infinity,
