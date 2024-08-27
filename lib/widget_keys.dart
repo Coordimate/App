b68547c63ce6dc0a9fb481c7a3830e841c4b087a
AFKabl3 <thisismyworkhotmail@gmail.com>
2024-08-27 04:54:22 +0300

    added some keys and ran build_runner (committing in case of legacy switches)

--- lib/widget_keys.dart
+++ lib/widget_keys.dart
@@ -19,6 +19,8 @@ const meetingsNavigationButtonKey = Key('meetingsNavigation');
 const groupsNavigationButtonKey = Key('groupsNavigation');
 const archiveButtonKey = Key('archiveButton');
 const meetingsScrollViewKey = Key('meetingCard');
+
+//groups_page keys:
 const groupCreationWindowKey = Key('groupCreationWindow');
 const createGroupKey = Key('createGroup');
 const groupCardKey = Key('groupCard');
@@ -26,6 +28,24 @@ const groupCardDescriptionKey = Key('groupCardDescription');
 const groupCardNameKey = Key('groupCardName');
 const groupCreationNameFieldKey = Key('groupCreationNameField');
 const groupCreationDescriptionFieldKey = Key(' groupCreationDescriptionField');
-const groupCardDescriptionOverflowKey = Key('groupCardDescriptionOverflow');
 const groupCardNameOverflowKey = Key('groupCardNameOverflow');
+const groupCardDescriptionOverflowKey = Key('groupCardDescriptionOverflow');
 const avatarKey = Key('avatar');
+
+//groups_details_page keys:
+//also check for avatar and archive buttons
+const inviteButtonKey = Key('inviteButton');
+const createMeetingButtonKey = Key('createMeetingButton');
+const editGroupNameKey = Key('editGroupName');
+const copyButtonKey = Key('copyButton');
+const shareButtonKey = Key('shareButton');
+const editGroupDescriptionKey = Key('editGroupDescription');
+const groupsDetailsNameKey = Key('groupsDetailsName');
+const groupsDetailsDescriptionKey = Key('groupsDetailsDescription');
+const createGroupPollButtonKey = Key('createGroupPollButton');
+const groupScheduleButtonKey = Key('groupScheduleButton');
+const groupMembersListKey = Key('groupMembersList');
+const groupMemberKey = Key('groupMember');
+const groupAdminKey = Key('groupAdmin');
+const groupChatButtonKey = Key('groupChatButton');
+const deleteGroupButtonKey = Key('deleteGroupButton');
--- test/groups_details_page_widget_test.dart
+++ test/groups_details_page_widget_test.dart
@@ -1,1 +1,31 @@
+import 'package:coordimate/app_state.dart';
+import 'package:coordimate/pages/groups_page.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'groups_page_widget_test.mocks.dart';
+import 'helpers/set_appstate.dart';
+import 'helpers/when.dart';
+import 'helpers/client/groups.dart';
+import 'helpers/client/data_provider.dart';
+import 'package:coordimate/widget_keys.dart';
+import 'package:mockito/mockito.dart';
+import 'package:coordimate/keys.dart';
 
+@GenerateMocks(
+    [http.Client, FlutterSecureStorage, SharedPreferences, FirebaseMessaging])
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  final firebase = MockFirebaseMessaging();
+  final client = MockClient();
+  final storage = MockFlutterSecureStorage();
+  final sharedPrefs = MockSharedPreferences();
+
+  setAppState(client, storage, sharedPrefs, firebase);
+  whenStatements(client, storage, sharedPrefs, firebase);
+}
