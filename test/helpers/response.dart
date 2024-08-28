c6e904d3937f37165a103881eb71340cefe520f2
Valentin Vikhorev <vvihorevv@gmail.com>
2024-08-28 02:55:23 +0200

    Oops, push response helperse for schedule controller test too

---
+++ test/helpers/response.dart
@@ -0,0 +1,56 @@
+import 'package:coordimate/keys.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+
+void getResponse(client, String url, String response, {int statusCode = 200}) {
+  when(client.get(
+    Uri.parse('$apiUrl$url'),
+    headers: anyNamed('headers'),
+  )).thenAnswer((_) async => http.Response(response, statusCode));
+}
+
+void patchResponse(client, String url, String response,
+    {int statusCode = 200}) {
+  when(client.patch(
+    Uri.parse('$apiUrl$url'),
+    headers: anyNamed('headers'),
+    body: anyNamed('body'),
+  )).thenAnswer((_) async => http.Response(response, statusCode));
+}
+
+void patchBodyResponse(client, String url, Map<String, dynamic> body, String response,
+    {int statusCode = 200}) {
+  when(client.patch(
+    Uri.parse('$apiUrl$url'),
+    headers: anyNamed('headers'),
+    body: body,
+  )).thenAnswer((_) async => http.Response(response, statusCode));
+}
+
+void deleteResponse(client, String url, String response,
+    {int statusCode = 204}) {
+  when(client.delete(
+    Uri.parse('$apiUrl$url'),
+    headers: anyNamed('headers'),
+  )).thenAnswer((_) async => http.Response(response, statusCode));
+}
+
+void postBodyResponse(
+    client, String url, Map<String, dynamic> body, String response,
+    {int statusCode = 200}) {
+  when(client.post(
+    Uri.parse('$apiUrl$url'),
+    headers: anyNamed('headers'),
+    body: body,
+  )).thenAnswer((_) async => http.Response(response, 200));
+}
+
+void postResponse(
+    client, String url, String response,
+    {int statusCode = 200}) {
+  when(client.post(
+    Uri.parse('$apiUrl$url'),
+    headers: anyNamed('headers'),
+    body: anyNamed('body'),
+  )).thenAnswer((_) async => http.Response(response, 200));
+}
